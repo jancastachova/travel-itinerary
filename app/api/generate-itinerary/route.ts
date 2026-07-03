c37466e6dcb0fca298529fa9217ef449fc981abe
jancastachova <jana.stachova1@seznam.cz>
2026-07-03 17:38:12 +0100

    add generate-itinerary API route

---
+++ app/api/generate-itinerary/route.ts
@@ -0,0 +1,25 @@
+import "dotenv/config"
+import { PrismaClient } from '../../../app/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { scoreDestinations } from "../../../src/lib/scoring"
+import { NextResponse } from "next/server"
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const prisma = new PrismaClient({ adapter })
+
+export async function POST(request: Request){
+    try{
+        const body = await request.json()
+        const destinations = await prisma.destination.findMany({
+            include: { tags: true }
+        })
+        const winner = scoreDestinations(body, destinations)
+        return NextResponse.json({ destination: winner })
+    }catch(error){
+        console.error(error)
+        return NextResponse.json(
+            { error: 'Something went wrong' },
+            { status: 500 }
+        )
+    }
+}
--- src/lib/scoring.ts
+++ src/lib/scoring.ts
@@ -1,6 +1,3 @@
-// takes imput from the user - array with an objects
-// import from the databases with all places
-
 //loops through every destionation and gives it a score based on how well it matches the user's answers
 
 type UserAnswers = {
