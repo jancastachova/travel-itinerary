e334ac642764cffe16c5c2f22d1c63daf781b412
jancastachova <jana.stachova1@seznam.cz>
2026-07-03 10:19:45 +0100

    setup prisma schema, seed data and preparation of scoring engine stub

---
+++ src/lib/scoring.ts
@@ -0,0 +1,8 @@
+// takes imput from the user - array with an objects
+// import from the databases with all places
+
+//loops through every destionation and gives it a score based on how well it matches the user's answers
+
+
+
+console.log("hello world")
