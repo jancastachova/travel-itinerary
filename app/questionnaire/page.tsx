eac553bff5f52c8e95f77886d49a72f6c585e22b
jancastachova <jana.stachova1@seznam.cz>
2026-07-06 09:31:55 +0100

    small ui features and testing the object

--- app/questionnaire/page.tsx
+++ app/questionnaire/page.tsx
@@ -1,37 +1,61 @@
-"use client"
-import { useState } from 'react'
+"use client";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  
-  const [currentStep, setCurrentStep] = useState(0)
+  const [currentStep, setCurrentStep] = useState(0);
   const [answers, setAnswers] = useState({
     days: 0,
-    budget:'',
+    budget: "",
     preferredTags: [] as string[],
-    climate: ''
-  })
+    climate: "",
+  });
   const [days, setDays] = useState<number>(5);
-  const activities = ['food','beaches','nighlife','culture','hiking','adventure','museums'];
+  const activities = [
+    "food",
+    "beaches",
+    "nighlife",
+    "culture",
+    "hiking",
+    "adventure",
+    "museums",
+  ];
+
+  useEffect(() => {
+    console.log(answers);
+  }, [answers]);
 
   return (
-    
-    <div className='flex flex-row items-center justify-center min-h-screen'>
-      
-      
-
+    <div className="flex flex-row items-center justify-center min-h-screen">
       {currentStep === 0 && (
         <div>
           <h1>How many days?</h1>
-          
-          <p>{days} days</p>
-          <input type="range" min={3} max={10} value={days} onChange={(e) => setDays(Number(e.target.value))}/>
 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" 
-              onClick={()=> setCurrentStep(currentStep - 1)}>Prev</button>
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, days: days })
-            setCurrentStep(1)
-          }}>Next</button> 
+          <p>{days} days</p>
+          <input
+            type="range"
+            min={3}
+            max={10}
+            value={days}
+            onChange={(e) => setDays(Number(e.target.value))}
+          />
+
+          
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, days: days });
+              setCurrentStep(1);
+            }}
+          >
+            Next
+          </button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => setCurrentStep(currentStep - 1)}
+          >
+            &lt;
+          </button>
+
           <h1>Step: {currentStep}</h1>
         </div>
       )}
@@ -39,94 +63,171 @@ export default function Home() {
       {currentStep === 1 && (
         <div>
           <h1>What's your budget?</h1>
-          
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, budget: 'HIGH' })
-            setCurrentStep(2)
-          }}>High</button> 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, budget: 'MEDIUM' })
-            setCurrentStep(2)
-          }}>Medium</button> 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, budget: 'LOW' })
-            setCurrentStep(2)
-          }}>Low</button> 
 
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, budget: "HIGH" });
+              setCurrentStep(2);
+            }}
+          >
+            High
+          </button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, budget: "MEDIUM" });
+              setCurrentStep(2);
+            }}
+          >
+            Medium
+          </button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, budget: "LOW" });
+              setCurrentStep(2);
+            }}
+          >
+            Low
+          </button>
 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" 
-              onClick={()=> setCurrentStep(currentStep - 1)}>Prev</button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => setCurrentStep(currentStep - 1)}
+          >
+            &lt;
+          </button>
           <h1>Step: {currentStep}</h1>
         </div>
       )}
 
-
       {currentStep === 2 && (
         <div>
           <h1>Do you prefer city or nature?</h1>
-          
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, preferredTags:['city']})
-            setCurrentStep(3)
-          }}>City</button> 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, preferredTags:['nature']})
-            setCurrentStep(3)
-          }}>Nature</button> 
 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" 
-              onClick={()=> setCurrentStep(currentStep - 1)}>Prev</button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, preferredTags: ["city"] });
+              setCurrentStep(3);
+            }}
+          >
+            City
+          </button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, preferredTags: ["nature"] });
+              setCurrentStep(3);
+            }}
+          >
+            Nature
+          </button>
+
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => setCurrentStep(currentStep - 1)}
+          >
+            &lt;
+          </button>
           <h1>Step: {currentStep}</h1>
         </div>
       )}
 
       {currentStep === 3 && (
-  <div>
-    <h1>What do you enjoy?</h1>
-    {activities.map(a => (
-      <div key={a}>
-        <input type='checkbox' id={a} value={a} onChange={(e) => {
-          if (e.target.checked) {
-            setAnswers({ ...answers, preferredTags: [...answers.preferredTags, a] })
-          } else {
-            setAnswers({ ...answers, preferredTags: answers.preferredTags.filter(t=> t !== a) })
-          }
-        }} />
-        <label htmlFor={a}>{a}</label>
-      </div>
-    ))}
+        <div>
+          <h1>What do you enjoy?</h1>
+          {activities.map((a) => (
+            <div key={a}>
+              <input
+                type="checkbox"
+                id={a}
+                value={a}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setAnswers({
+                      ...answers,
+                      preferredTags: [...answers.preferredTags, a],
+                    });
+                  } else {
+                    setAnswers({
+                      ...answers,
+                      preferredTags: answers.preferredTags.filter(
+                        (t) => t !== a,
+                      ),
+                    });
+                  }
+                }}
+              />
+              <label htmlFor={a}>{a}</label>
+            </div>
+          ))}
 
-    <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" 
-      onClick={() => setCurrentStep(currentStep - 1)}>Prev</button>
-    <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" 
-      onClick={() => setCurrentStep(4)}>Next</button>
-    <h1>Step: {currentStep}</h1>
-  </div>
-)}
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => setCurrentStep(4)}
+          >
+            Next
+          </button>
+
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => setCurrentStep(currentStep - 1)}
+          >
+            &lt;
+          </button>
+          <h1>Step: {currentStep}</h1>
+        </div>
+      )}
 
       {currentStep === 4 && (
         <div>
           <h1>What climate?</h1>
-          
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, climate: 'Arctic' })
-            setCurrentStep(5)
-          }}>Arctic❄️ </button> 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, climate: 'Tropical' })
-            setCurrentStep(5)
-          }}>Tropical🌴</button> 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, climate: 'Mediterranean' })
-            setCurrentStep(5)
-          }}>Mediterranean☀️</button>
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" onClick={() => {
-            setAnswers({ ...answers, climate: 'Continental' })
-            setCurrentStep(5)
-          }}>Continental🍂 </button>  
 
-          <button className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition" 
-              onClick={()=> setCurrentStep(currentStep - 1)}>Prev</button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, climate: "Arctic" });
+              setCurrentStep(5);
+            }}
+          >
+            Arctic❄️{" "}
+          </button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, climate: "Tropical" });
+              setCurrentStep(5);
+            }}
+          >
+            Tropical🌴
+          </button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, climate: "Mediterranean" });
+              setCurrentStep(5);
+            }}
+          >
+            Mediterranean☀️
+          </button>
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => {
+              setAnswers({ ...answers, climate: "Continental" });
+              setCurrentStep(5);
+            }}
+          >
+            Continental🍂{" "}
+          </button>
+
+          <button
+            className="mr-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
+            onClick={() => setCurrentStep(currentStep - 1)}
+          >
+            &lt;
+          </button>
           <h1>Step: {currentStep}</h1>
         </div>
       )}
@@ -155,5 +256,5 @@ export default function Home() {
       <button className="ml-4 bg-yellow-400 text-black font-semibold px-8 py-4 rounded-full hover:bg-yellow-300 transition"
               onClick={()=> setCurrentStep(currentStep + 1)}>Next</button> */}
     </div>
-  )
+  );
 }
