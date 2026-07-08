d28183ce1868b2199d9c786cccfaa9900c5e14eb
jancastachova <jana.stachova1@seznam.cz>
2026-07-08 14:23:00 +0100

    fix PrismaClient import in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
-import { PrismaClient } from '../app/generated/prisma/client'
+// import { PrismaClient } from '../app/generated/prisma/client'
+import { PrismaClient } from "@prisma/client"
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
